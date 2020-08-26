d533099c0b15ba764b755249d6c1e12e0406d61a
Inez Escandon <iescan4@gmail.com>
2020-08-26 17:23:34 -0500

    carets added but not working properly

--- src/components/employeerow/index.js
+++ src/components/employeerow/index.js
@@ -1,16 +1,104 @@
 import React from 'react';
 
-function Employee({ employees }) {
+function Employee({ employees, tableHeaders, order, handleOrder }) {
+	// const image = order[0].Image;
+	// const firstName = order[1]['First Name'];
+	// const lastName = order[2]['Last Name'];
+	// const phoneNumber = order[3]['Phone Number'];
+	// const email = order[4]['E-mail'];
+
+	// let orderObj = {
+	// 	Image: order,
+	// 	'First Name': order,
+	// 	'Last Name': order,
+	// 	'Phone Number': order,
+	// 	'E-mail': order,
+	// };
+
+	// const handleOrder = (header) => {
+	// 	console.log(header);
+	// 	for (var i = 0; i < tableHeaders.length; i++) {
+	// 		const currentHeader = tableHeaders[i].name;
+	// 		if (header === currentHeader) {
+	// 			if (orderObj[header] === 'down') {
+	// 				console.log(orderObj[header]);
+	// 				orderObj[header] = 'up';
+	// 			} else {
+	// 				console.log(orderObj[header]);
+	// 				orderObj[header] = 'down';
+	// 			}
+	// 		}
+	// 	}
+	// };
+
 	return (
 		<table className="table table-striped mt-0">
 			<thead>
 				<tr>
-					<th scope="col"></th>
-					<th scope="col">First</th>
-					<th scope="col">Last</th>
-					<th scope="col">Phone Number</th>
-					<th scope="col">E-mail</th>
+					{tableHeaders.map((header) => {
+						return (
+							<th key={header.name}>
+								{header.name}
+								<i
+									className={`fa fa-caret-${order}`}
+									onClick={() => handleOrder(header.name)}
+								></i>
+							</th>
+						);
+					})}
 				</tr>
+
+				{/* <tr>
+					{tableHeaders.map((header) => {
+						return (
+							<th key={header.name}>
+								{header.name}
+								<i
+									className={`fa fa-caret-${orderObj[header.name]}`}
+									onClick={() => handleOrder(header.name)}
+								></i>
+							</th>
+						);
+					})}
+				</tr> */}
+
+				{/* <tr>
+					<th>
+						Image
+						<i
+							className={`fa fa-caret-${image}`}
+							onClick={() => handleOrder(tableHeaders[0].name)}
+						></i>
+					</th>
+					<th>
+						First Name
+						<i
+							className={`fa fa-caret-${firstName}`}
+							onClick={() => handleOrder(tableHeaders[1].name)}
+						></i>
+					</th>
+					<th>
+						Last Name
+						<i
+							className={`fa fa-caret-${lastName}`}
+							onClick={() => handleOrder(tableHeaders[2].name)}
+						></i>
+					</th>
+					<th>
+						Phone Number
+						<i
+							className={`fa fa-caret-${phoneNumber}`}
+							onClick={() => handleOrder(tableHeaders[3].name)}
+						></i>
+					</th>
+					<th>
+						E-mail
+						<i
+							className={`fa fa-caret-${email}`}
+							onClick={() => handleOrder(tableHeaders[4].name)}
+						></i>
+					</th>
+				</tr> */}
 			</thead>
 			<tbody>
 				{employees.map((employee) => {
--- src/components/searchbar/index.js
+++ src/components/searchbar/index.js
@@ -1,62 +1,36 @@
-// import React, { useState, useEffect } from 'react';
-// import EmployeeRow from '../employeerow';
-// import API from '../../utils/API';
-
-// function Search() {
-// 	const [state, setState] = useState({
-// 		search: '',
-// 		results: [],
-// 	});
-
-// 	useEffect(() => {
-// 		API.search().then((res) => {
-// 			setState({ ...state, results: res.data });
-// 			// console.log(res.data);
-// 			console.log(state);
-// 		});
-// 	}, [state.results]);
-
-// 	//  handleInputChange = (event) => {
-// 	// 		const name = event.target.name;
-// 	// 		const value = event.target.value;
-// 	// 		setState({
-// 	// 			[name]: value,
-// 	// 		});
-// 	// 	};
-
-// 	return (
-// 		<div>
-// 			<div className="jumbotron mb-0">
-// 				<h1 className="display-4 mb-5 text-center">Employee Directory</h1>
-
-// 				<form className="form-inline my-2 my-lg-0 justify-content-center">
-// 					<input
-// 						className="form-control mr-sm-2"
-// 						type="search"
-// 						placeholder="Search"
-// 						aria-label="Search"
-// 					/>
-// 				</form>
-// 			</div>
-// 			<EmployeeRow results={state.results} />
-// 		</div>
-// 	);
-// }
-
-// export default Search;
-
-// -------------------------------------
-
 import React, { Component } from 'react';
 import EmployeeRow from '../employeerow';
 import API from '../../utils/API';
+// import BootstrapTable from 'react-bootstrap-table-next';
 
 class Search extends Component {
 	state = {
 		search: '',
 		results: [],
 		filteredResults: [],
+		order: 'down',
 	};
+
+	// state = {
+	// 	search: '',
+	// 	results: [],
+	// 	filteredResults: [],
+	// 	order: [
+	// 		{ Image: 'down' },
+	// 		{ 'First Name': 'down' },
+	// 		{ 'Last Name': 'down' },
+	// 		{ 'Phone Number': 'down' },
+	// 		{ 'E-mail': 'down' },
+	// 	],
+	// };
+
+	tableHeaders = [
+		{ name: 'Image', sort: true },
+		{ name: 'First Name', sort: true },
+		{ name: 'Last Name', sort: true },
+		{ name: 'Phone Number', sort: true },
+		{ name: 'E-mail', sort: true },
+	];
 
 	componentDidMount() {
 		this.getUsers();
@@ -65,24 +39,20 @@ class Search extends Component {
 	getUsers = () => {
 		API.search().then((res) => {
 			this.setState({
-				...this.state,
 				results: res.data.results,
+				filteredResults: res.data.results,
 			});
 		});
 	};
 
-	updateResults = () => {
-		//algorithm   .filter, .sort
-		this.setState({ filteredResults: [] });
+	updateResults = (event) => {
+		const filterSearch = this.state.search;
+		const filteredList = this.state.results.filter((item) => {
+			let values = Object.values(item).join('').toLowerCase();
+			return values.indexOf(filterSearch.toLowerCase()) !== -1;
+		});
+		this.setState({ filteredResults: filteredList });
 	};
-	// componentDidMount() {
-	//   API.search().then(results => {
-	//     this.setState({
-	//       users: results.data.results,
-	//       filteredUsers: results.data.results
-	//     });
-	//   });
-	// }
 
 	handleInputChange = (event) => {
 		const name = event.target.name;
@@ -90,24 +60,54 @@ class Search extends Component {
 		this.setState({
 			[name]: value,
 		});
+		this.updateResults();
+	};
+
+	handleOrder = (header) => {
+		console.log(header);
+		for (var i = 0; i < this.tableHeaders.length; i++) {
+			const currentHeader = this.tableHeaders[i].name;
+			console.log(currentHeader);
+			if (header === currentHeader) {
+				if (this.state.order === 'down') {
+					this.setState({
+						order: 'up',
+					});
+				} else {
+					this.setState({
+						order: 'down',
+					});
+				}
+			}
+		}
+		console.log(header);
+		// set state filtered search
 	};
 
 	render() {
 		return (
 			<div>
 				<div className="jumbotron mb-0">
-					<h1 className="display-4 mb-5 text-center">Employee Directory</h1>
-
+					<h1 className="display-4 mb-5 text-center">EMPLOYEE DIRECTORY</h1>
+					{/* <h2>{this.state.search}</h2> */}
 					<form className="form-inline my-2 my-lg-0 justify-content-center">
 						<input
+							name="search"
+							value={this.state.search}
 							className="form-control mr-sm-2"
 							type="search"
 							placeholder="Search"
 							aria-label="Search"
+							onChange={this.handleInputChange}
 						/>
 					</form>
 				</div>
-				<EmployeeRow employees={this.state.results} />
+				<EmployeeRow
+					employees={this.state.filteredResults}
+					tableHeaders={this.tableHeaders}
+					order={this.state.order}
+					handleOrder={this.handleOrder}
+				/>
 			</div>
 		);
 	}
