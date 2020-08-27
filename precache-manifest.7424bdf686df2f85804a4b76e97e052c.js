01c8f52ff76a5823876c5a7f80e42842012001dc
Inez Escandon <iescan4@gmail.com>
2020-08-26 21:39:28 -0500

    Updates

---
+++ precache-manifest.7424bdf686df2f85804a4b76e97e052c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49abf2c7b70dfba860fbb9b55aef98aa",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "ee00ad4e85724af78bcf",
+    "url": "/employee-directory/static/css/main.f82d690a.chunk.css"
+  },
+  {
+    "revision": "ceab6f91d154e5844d09",
+    "url": "/employee-directory/static/js/2.4588f09d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.4588f09d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee00ad4e85724af78bcf",
+    "url": "/employee-directory/static/js/main.23939a0c.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
